7a0329136889c7dee633da2858f11102d031dbb7
Thiago Andrade - Grupo DPSP <thiago.andrade@dpsp.com.br>
2022-10-11 18:30:48 -0300

    ajustando dataJpaTest para dataMongoTest

--- src/test/java/com/getnet/getnet/repository/VoucherRepositoryTest.java
+++ src/test/java/com/getnet/getnet/repository/VoucherRepositoryTest.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 
-@DataJpaTest
+@DataMongoTest
 @ExtendWith(SpringExtension.class)
 class VoucherRepositoryTest {
 
